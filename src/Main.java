4099371d26579b12b451ff36c80a0be1cac097c4
banerjri <banerjri@mail.uc.edu>
2023-07-01 10:18:03 -0500

    game done loop bullet proof

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String Choice = "";
         String Party = "Y - yes\tN - No = ";
@@ -12,43 +11,46 @@ public class Main {
         Choice = in.nextLine();
         if (Choice.equalsIgnoreCase("Y"))
         {
-        Random rnd = new Random();
-        int die1 = rnd.nextInt(6)+1;
-        int die2 = rnd.nextInt(6)+1;
-        final int firstSum = die1 + die2;
-        System.out.printf("%d + %d = %d %n", die1, die2, firstSum);
+            Random rnd = new Random();
+            int die1 = rnd.nextInt(6) + 1;
+            int die2 = rnd.nextInt(6) + 1;
+            final int firstSum = die1 + die2;
+            System.out.printf("%d + %d = %d %n", die1, die2, firstSum);
 
-        if (firstSum == 7 || firstSum == 11)
-        {
-            System.out.println("You win.");
-            return;
-        }
-        if (firstSum == 2 || firstSum == 3 || firstSum == 12)
-        {
-            System.out.println("You lose.");
-            return;
-        }
-
-        int newSum = 0;
-        while (newSum != firstSum && newSum != 7)
-        {
-            die1 = rnd.nextInt(6)+1;
-            die2 = rnd.nextInt(6)+1;
-            newSum = die1 + die2;
-            System.out.printf("%d + %d = %d %n", die1, die2, newSum);
-        }
-
-        if (newSum == firstSum) {
-            System.out.println("You win.");
-        } else {
-            System.out.println("You lose.");
-        }
-        System.out.println("Made a point = " +newSum);
-        if(Choice.equalsIgnoreCase("N"))
-            {
-                System.out.println("the game over");
+            if (firstSum == 7 || firstSum == 11) {
+                System.out.println("You win.");
+                return;
             }
-        }
+            if (firstSum == 2 || firstSum == 3 || firstSum == 12) {
+                System.out.println("You lose.");
+                return;
+            }
 
+            int newSum = 0;
+            while (newSum != firstSum && newSum != 7) {
+                die1 = rnd.nextInt(6) + 1;
+                die2 = rnd.nextInt(6) + 1;
+                newSum = die1 + die2;
+                System.out.printf("%d + %d = %d %n", die1, die2, newSum);
+            }
+
+            if (newSum == firstSum) {
+                System.out.println("You win.");
+            }
+            else {
+                System.out.println("You lose.");
+            }
+            System.out.println("Made a point = " + newSum);
+
+
+        }
+        if (Choice.equalsIgnoreCase("N"))
+        {
+            System.out.println("Stop game");
+        }
+        else
+        {
+            System.out.println("wrong input");
+        }
     }
 }
