46fdb03677f18b2b4f9610a54a80d14257753a9a
banerjri <banerjri@mail.uc.edu>
2023-07-01 09:29:26 -0500

    game done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Random;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args)
+    {
+        Random rnd = new Random();
+        int die1 = rnd.nextInt(6)+1;
+        int die2 = rnd.nextInt(6)+1;
+        final int firstSum = die1 + die2;
+        System.out.printf("%d + %d = %d %n", die1, die2, firstSum);
+
+        if (firstSum == 7 || firstSum == 11)
+        {
+            System.out.println("You win.");
+            return;
+        }
+        if (firstSum == 2 || firstSum == 3 || firstSum == 12)
+        {
+            System.out.println("You lose.");
+            return;
+        }
+
+        int newSum = 0;
+        while (newSum != firstSum && newSum != 7)
+        {
+            die1 = rnd.nextInt(6)+1;
+            die2 = rnd.nextInt(6)+1;
+            newSum = die1 + die2;
+            System.out.printf("%d + %d = %d %n", die1, die2, newSum);
+        }
+
+        if (newSum == firstSum) {
+            System.out.println("You win.");
+        } else {
+            System.out.println("You lose.");
+        }
+        System.out.println("Made a point = " +newSum);
     }
 }
